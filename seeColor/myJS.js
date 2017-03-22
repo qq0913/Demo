c3622a7aedc7831ab719529a37af8a1d6cf47067
qq0913 <qq_0913@sina.com>
2017-03-22 12:19:21 +0800

    show see color

---
+++ seeColor/myJS.js
@@ -0,0 +1,72 @@
+/**
+ * Created by Qiaoqiao on 2017/3/22.
+ */
+var stage = new createjs.Stage("gameView");
+createjs.Ticker.setFPS(20);
+createjs.Ticker.addEventListener("tick",stage);
+var gameView = new createjs.Container();
+stage.addChild(gameView);
+var n=2;
+var level=1;
+var b=15;
+var k=33;
+var start_flag = false;
+function addRect(start_flag){
+    var range=140;
+    var light=60;
+    var color="#";
+    var diffcolor="#";
+    var tt=Math.random();
+    for (var i=0;i<3;i++){
+        var cl=parseInt(Math.random()*range)+light;
+        var tmp=parseInt(k/level+b);
+        var dcl=tt>0.5?cl+tmp:cl-tmp;
+        cl = cl.toString(16);
+        dcl = dcl.toString(16);
+        if(cl.length<2){
+            cl="0"+cl;
+        }
+        if(dcl.length<2){
+            dcl="0"+dcl;
+        }
+        color=color+cl;
+        diffcolor=diffcolor+dcl;
+    }
+    var x=parseInt(Math.random()*n);
+    var y=parseInt(Math.random()*n);
+    for(var indexX=0;indexX<n;indexX++){
+        for(var indexY=0;indexY<n;indexY++){
+            var r=new Rect(n,color,diffcolor);
+            gameView.addChild(r);
+            r.x=indexX;
+            r.y=indexY;
+            if(r.x==x && r.y==y){
+                r.setRectType(2);
+            }
+            r.x=indexX*400/n;
+            r.y=indexY*400/n;
+            if(start_flag == true &&r.getRectType()==2){
+                r.addEventListener("click",function(){
+                    if(n<7){
+                        ++n;
+                    }
+                    gameView.removeAllChildren();
+                    level++;
+                    document.getElementById("level").innerHTML=level;
+                    addRect(start_flag);
+                })
+            }
+        }
+    }
+}
+var btn = document.getElementById("btn");
+btn.onclick = function(){
+    btn.innerHTML = "Restart";
+    n = 2;
+    level = 1;
+    start_flag = true;
+    document.getElementById("level").innerHTML=level;
+    gameView.removeAllChildren();
+    addRect(start_flag);
+}
+addRect(start_flag);
